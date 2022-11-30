7d68ef5ba5f9653b603b72ae5028157cbfdfe73c
Jake Glenn <jbradleyglenn@gmail.com>
2022-11-29 19:42:24 -0500

    trie test

--- more_collections/Trie.py
+++ more_collections/Trie.py
@@ -10,13 +10,28 @@ class Trie:
         self.size = 0
     
     def insert(self, word):
+        self.depth = max(self.depth, len(word))
+        self.size += 1
         node = self.root
         for letter in word:
             if letter not in node.children:
-                node.children[c] = TrieNode()
-            node = node.children[c]
+                node.children[letter] = TrieNode()
+            node = node.children[letter]
         node.is_word = True
 
-    # def delete(self, word):
-    #     node = self.root
+    def delete(self, word):
+        nodes = [self.root]
+        node = self.root
+        for letter in word:
+            if letter not in node.children:
+                return
+            nodes.append(node.children[letter])
+            node = node.children[letter]
+        self.size -= 1
+        for i in range(len(nodes) - 1, 0, -1):
+            if nodes[i].is_word or nodes[i].children:
+                return
+            else:
+                del nodes[i - 1].children[word[i-1]]
+
 
