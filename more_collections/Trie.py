ffdf7dac67c86c0ae0775090c7545d194f60ec28
Jake Glenn <jbradleyglenn@gmail.com>
2022-11-29 19:15:54 -0500

    start trie

--- more_collections/Trie.py
+++ more_collections/Trie.py
@@ -1,2 +1,22 @@
 class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        self.depth = 0
+        self.size = 0
     
+    def insert(self, word):
+        node = self.root
+        for letter in word:
+            if letter not in node.children:
+                node.children[c] = TrieNode()
+            node = node.children[c]
+        node.is_word = True
+
+    # def delete(self, word):
+    #     node = self.root
+
